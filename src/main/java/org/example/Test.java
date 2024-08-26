ff4a2a8579c5bd6ee15d2f263d33bc957f112747
Parami <paramiashinsana@gmail.com>
2024-08-26 18:31:24 +0530

    Test 01

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input Your Number : ");
+        double number = scanner.nextDouble();
+
+        double inch = number*25.4;
+        System.out.println(inch+" inch");
+    }
+}
